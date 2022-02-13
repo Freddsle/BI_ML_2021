bba2d6a07c084dd8a44e424e746397716c99bcdd
Yuliya Burankova (Freddsle) <burankova.y@gmail.com>
2022-02-13 16:53:34 +0100

    multiclass added

--- l01_knn/code/metrics.py
+++ l01_knn/code/metrics.py
@@ -16,10 +16,42 @@ def binary_classification_metrics(y_pred, y_true):
     # https://en.wikipedia.org/wiki/Precision_and_recall
     # https://en.wikipedia.org/wiki/F1_score
 
-    """
-    YOUR CODE IS HERE
-    """
-    pass
+
+    true_positive = 0
+    false_positive = 0
+    true_negative = 0
+    false_negative = 0
+
+    for i, pred in enumerate(y_pred):
+
+        if pred == y_true[i]:
+            if pred == 1:
+                true_positive += 1
+            if pred == 0:
+                true_negative += 1
+
+        elif pred != y_true[i]:
+            if pred == 1 and y_true[i] == 0:
+                false_positive += 1
+
+            elif pred == 0 and y_true[i] == 1:
+                false_negative += 1
+
+    accuracy = (true_positive + true_negative) / (true_positive + false_positive + true_negative + false_negative)
+
+    if true_positive == 0 and false_positive == 0 and false_negative == 0:
+        # https://github.com/dice-group/gerbil/wiki/Precision,-Recall-and-F1-measure
+        precision, recall, f1 = 1, 1, 1
+
+    elif true_positive == 0:
+        precision, recall, f1 = 0, 0, 0
+
+    else:
+        precision = true_positive / (true_positive + false_positive)
+        recall = true_positive / (true_positive + false_negative)
+        f1 = (2 * precision * recall) / (recall + precision)
+
+    return precision, recall, f1, accuracy
 
 
 def multiclass_accuracy(y_pred, y_true):
@@ -32,10 +64,20 @@ def multiclass_accuracy(y_pred, y_true):
     accuracy - ratio of accurate predictions to total samples
     """
 
-    """
-    YOUR CODE IS HERE
-    """
-    pass
+    true_values = 0
+    false_value = 0
+
+    for i, pred in enumerate(y_pred):
+
+        if pred == y_true[i]:
+            true_values += 1
+
+        else:
+            false_value += 1
+
+    accuracy = true_values / (true_values + false_value)
+
+    return accuracy
 
 
 def r_squared(y_pred, y_true):
